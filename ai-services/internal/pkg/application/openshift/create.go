f9e15150aa4f4d2d2e24e282794aca13318f9eb1
sagarwal-ibm <sagarwal@ibm.com>
2026-07-27 20:57:22 +0530

    modify rbac
    Signed-off-by: sagarwal-ibm <sagarwal@ibm.com>

--- ai-services/internal/pkg/application/openshift/create.go
+++ ai-services/internal/pkg/application/openshift/create.go
@@ -102,7 +102,6 @@ func deployApp(ctx context.Context, chart chart.Charter, timeout time.Duration, 
 	s := spinner.New("Deploying application '" + app + "'...")
 
 	s.Start(ctx)
-
 	// Create a new Helm client
 	helmClient, err := helm.NewHelm(namespace)
 	if err != nil {
